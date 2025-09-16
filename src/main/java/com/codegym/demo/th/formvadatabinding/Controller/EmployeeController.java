81260d4268e66fd2de96c51f7bf17fd9d662a01e
Dungndph20697 <dungthoai321@gmail.com>
2025-09-16 08:35:22 +0700

    th  Form và Data Binding

---
+++ src/main/java/com/codegym/demo/th/formvadatabinding/Controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.codegym.demo.th.formvadatabinding.Controller;
+
+import com.codegym.demo.th.formvadatabinding.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @GetMapping("")
+    public String showForm(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "/th/formvadatabinding/create";
+    }
+
+    @PostMapping("/create")
+    public String submit(@ModelAttribute("employee") Employee employee, Model model) {
+        model.addAttribute("employee", employee);
+        model.addAttribute("ContactNumber", employee.getContactNumber());
+        model.addAttribute("id", employee.getId());
+        return "/th/formvadatabinding/info";
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/views/th/formvadatabinding/info.jsp
@@ -0,0 +1,29 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 9/16/2025
+  Time: 8:26 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<table>
+  <tr>
+    <td>Name:</td>
+    <td>${name}</td>
+  </tr>
+  <tr>
+    <td>ID:</td>
+    <td>${id}</td>
+  </tr>
+  <tr>
+    <td>Contact Number:</td>
+    <td>${contactNumber}</td>
+  </tr>
+</table>
+</body>
+</html>
